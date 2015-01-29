c2619daaf111f6096f20d1f427ae7a11a6be1b95
Bryan Mackenzie <bmackenzie@my.devry.edu>
2015-01-29 14:52:02 -0600

    Revert "Detect and load Intel Power Gadget"
    This reverts commit a70236f684c9db5b8677a76ec4e4442ba1d970b0.

--- UniPower/Assets/Scripts/PowerGadgetUsage.cs
+++ UniPower/Assets/Scripts/PowerGadgetUsage.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Text;
 using UnityEngine;
 using System.Collections;
@@ -49,8 +48,9 @@ public class PowerGadgetUsage : MonoBehaviour
 	// Use this for initialization
 	void Start () 
     {
-        ///Check and Load the Power Gadget library
-        CheckLoadPowerGadget();
+        ///Load the Power Gadget library
+        //LoadNativeDll("C://Program Files (x86)//Intel//Power Gadget 3.0//EnergyLib32.dll");
+        LoadNativeDll("C:\\Program Files (x86)\\Intel\\Power Gadget 3.0\\EnergyLib32.dll");
 
         pInitialize = IntelEnergyLibInitialize();
         if (pInitialize != true)
@@ -59,7 +59,7 @@ public class PowerGadgetUsage : MonoBehaviour
         }
 
         pIsGTAvailable = IsGTAvailable();
-        if (pIsGTAvailable == true)
+        if (pIsGTAvailable != true)
         {
             pGetGTFrequency = GetGTFrequency(out pGTFreq);
             if (pGetGTFrequency == true)
@@ -116,24 +116,6 @@ public class PowerGadgetUsage : MonoBehaviour
     }
 
     /// <summary>
-    /// Checks if Intel Power Gadget is installed on the system, then loads it
-    /// </summary>
-    /// <returns></returns>
-    public bool CheckLoadPowerGadget()
-    {
-        string isInstalled = System.Environment.GetEnvironmentVariable("IPG_Dir");
-
-        if (isInstalled != null)
-        {
-            LoadNativeDll(isInstalled + "\\EnergyLib32.dll");
-            return true;
-        }
-
-        Debug.Log("Failed to locate/Load Power Gadget please check your install");
-        return false;
-    }
-
-    /// <summary>
     /// Load a native library
     /// </summary>
     /// <param name="FileName"></param>
@@ -151,6 +133,4 @@ public class PowerGadgetUsage : MonoBehaviour
             throw new Win32Exception();
         }
     }
-
-    
 }
